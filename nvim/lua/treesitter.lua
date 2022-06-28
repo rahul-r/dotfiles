efd4daa316e87a3a854fe757f4bdb141dac2ef1c
Rahul <mail@rahul.one>
2022-06-27 18:51:43 -0700

    Add rainbow brackets

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -24,7 +24,12 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enabled = true,
 	disable = {},
-  }
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+  },
 }
 
 -- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
