e13db89dee9a06721c30622a00c3287b8ab7d0a1
Rahul <mail@rahul.one>
2022-06-30 18:37:26 -0700

    Convert keymaps to lua

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,23 +1,21 @@
-vim.cmd([[
- " Change the leader key
-  let mapleader = " "
+-- Change the leader key
+vim.g.mapleader = " "
 
+vim.cmd([[
   " Enforce laziness
   "nnoremap <Left> :echo "Don't be silly, use h"<CR>
   "nnoremap <Down> :echo "Don't be silly, use j"<CR>
   "nnoremap <Up> :echo "Don't be silly, use k"<CR>
   "nnoremap <Right> :echo "Don't be silly, use l"<CR>
-
-  "split navigations
-  nnoremap <C-J> <C-W><C-J>
-  nnoremap <C-K> <C-W><C-K>
-  nnoremap <C-L> <C-W><C-L>
-  nnoremap <C-H> <C-W><C-H>
 ]])
 
-vim.cmd([[
-  nnoremap <leader>ff <cmd>Telescope find_files<cr>
-  nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-  nnoremap <leader>fb <cmd>Telescope buffers<cr>
-  nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-]])
+vim.keymap.set('n', '<C-J>', '<C-W><C-J>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-K>', '<C-W><C-K>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-L>', '<C-W><C-L>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-H>', '<C-W><C-H>', { noremap = true, silent = true })
+
+-- Telescope keymaps
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
