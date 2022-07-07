8076498a3d2f7e38ee6136a118e30f98c3f28cd1
Rahul <rahul>
2022-07-06 17:14:28 -0700

    Add telescope fzf native plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -141,6 +141,10 @@ return packer.startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  }
 
   -- Helpers
   use "windwp/nvim-autopairs"
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,26 +1,36 @@
-local present, telescope = pcall(require, "telescope")
+local telescope_ok, telescope = pcall(require, "telescope")
 
-if not present then
-    return
+if not telescope_ok then
+  return
 end
 
 telescope.setup {
-	defaults = {
-		layout_config = {
-			preview_cutoff = 1,
-			--layout_strategy = "flex",
-			width = .7,
-			preview_width = .7,
-		},
-		file_ignore_patterns = {
-			"^./.git/",
-			".cache",
-			"node_modules/",
-			"build/",
-			"build_.*/",
-			"dist/",
-			"%.o",
-			"%.a"
-		}
-	},
+  defaults = {
+    layout_config = {
+      preview_cutoff = 1,
+      --layout_strategy = "flex",
+      width = .7,
+      preview_width = .7,
+    },
+    file_ignore_patterns = {
+      "^./.git/",
+      ".cache",
+      "node_modules/",
+      "build/",
+      "build_.*/",
+      "dist/",
+      "%.o",
+      "%.a"
+    }
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"; the default case_mode is "smart_case"
+    }
+  }
 }
+
+telescope.load_extension('fzf') -- This require 'telescope-fzf-native.nvim' plugin
