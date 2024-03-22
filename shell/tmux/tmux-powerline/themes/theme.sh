fa999466fc33deadcc356902e0d46bd2462d667c
Rahul <rahul@Earth.starfleet>
2024-03-22 03:05:16 -0700

    Customize tmux to work better on mac

--- shell/tmux/tmux-powerline/themes/theme.sh
+++ shell/tmux/tmux-powerline/themes/theme.sh
@@ -26,10 +26,10 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 
 if [ -z $TMUX_POWERLINE_WINDOW_STATUS_CURRENT ]; then
 	TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
-		"#[$(format inverse)]" \
-		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR" \
-		" #I#F #W "\
-		"#[$(format regular)]" \
+		"#[$(format inverse)]"
+		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
+		" #I#F #W "
+		"#[$(format regular)]"
 		"$TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR"
 	)
 fi
@@ -42,8 +42,8 @@ fi
 
 if [ -z $TMUX_POWERLINE_WINDOW_STATUS_FORMAT ]; then
 	TMUX_POWERLINE_WINDOW_STATUS_FORMAT=(
-		"#[$(format regular)]" \
-		"  #I#{?window_flags,#F, } #W " \
+		"#[$(format regular)]"
+		"  #I#{?window_flags,#F, } #W "
 		""
 	)
 fi
@@ -75,25 +75,25 @@ fi
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 11 234" \
+		"tmux_session_info 11 234"
 		# "hostname 33 0" \
-		# "vcs_branch 29 88" \
-		# "vcs_compare 60 255" \
-		#"vcs_staged 64 255" \
-		#"vcs_modified 9 255" \
-		# "vcs_others 245 0" \
+		# "vcs_branch 29 88"
+		# "vcs_compare 60 255"
+		# "vcs_staged 64 255"
+		# "vcs_modified 9 255"
+		# "vcs_others 245 0"
 	)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		"pwd 89 211" \
-		"now_playing 234 37" \
+		"pwd 89 211"
+		# "now_playing 234 37"
 		# "cpu 240 136" \
 		# "tmux_mem_cpu_load 234 136" \
 		# "battery 137 127" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		# "date_day 235 136" \
+		# "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		# "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
