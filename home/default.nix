f2cfe8a244d2fc5e9ed247667d24082b7cdd6a09
Rahul <mail@rahul.one>
2024-04-19 15:43:47 -0700

    Add delta git diff tool and configure lazygit to use delta to show diff

--- home/default.nix
+++ home/default.nix
@@ -34,7 +34,7 @@
     lemonade
     cppcheck
     ripgrep
-    lazygit
+    delta
     lf
     ranger
     btrfs-assistant
@@ -62,8 +62,36 @@
     userEmail = "jossb6cw@duck.com";
     lfs.enable = true;
     extraConfig = {
-        core = { editor = "vi"; };
+        core = {
+          editor = "vi";
+          pager = "delta";
+        };
+        interactive = {
+          diffFilter = "delta --color-only";
+        };
+        delta = {
+          navigate = true;
+          features = ["line-numbers" "side-by-side"];
+        };
+        merge = {
+          conflictstyle = "diff3";
+        };
+        diff = {
+          colorMoved = "default";
+        };
         pull = { rebase = true; };
+    };
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      "git" = {
+        "paging" = {
+            "colorArg" = "always";
+            "pager" = "delta --dark --paging=never";
+        };
+      };
     };
   };
 
