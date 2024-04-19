5210a3d9cce74ab41416b6c3d06ac497d763138b
Rahul <mail@rahul.one>
2024-04-19 16:38:23 -0700

    Move git config from default.nix to a separate nix file

--- home/default.nix
+++ home/default.nix
@@ -13,6 +13,10 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
+  imports = [
+    ./git.nix
+  ];
+
   home.packages = with pkgs; [
     (pkgs.nerdfonts.override { fonts = [ "DroidSansMono" ]; })
 
@@ -34,7 +38,6 @@
     lemonade
     cppcheck
     ripgrep
-    delta
     lf
     ranger
     btrfs-assistant
@@ -56,45 +59,6 @@
     nodePackages.neovim
   ];
   
-  programs.git = {
-    enable = true;
-    userName  = "Rahul";
-    userEmail = "jossb6cw@duck.com";
-    lfs.enable = true;
-    extraConfig = {
-        core = {
-          editor = "vi";
-          pager = "delta";
-        };
-        interactive = {
-          diffFilter = "delta --color-only";
-        };
-        delta = {
-          navigate = true;
-          features = ["line-numbers" "side-by-side"];
-        };
-        merge = {
-          conflictstyle = "diff3";
-        };
-        diff = {
-          colorMoved = "default";
-        };
-        pull = { rebase = true; };
-    };
-  };
-
-  programs.lazygit = {
-    enable = true;
-    settings = {
-      "git" = {
-        "paging" = {
-            "colorArg" = "always";
-            "pager" = "delta --dark --paging=never";
-        };
-      };
-    };
-  };
-
   programs.zoxide.enable = true;
 
   programs.firefox = {
