9a85823e9a254676c5711fcb84c8255678e562c0
Rahul <mail@rahul.one>
2024-04-24 05:27:40 -0700

    Move common items to utils.nix

--- home/linux.nix
+++ home/linux.nix
@@ -8,19 +8,11 @@
   ...
 }:
 let
-  nixGLWrap =
-    pkg:
-    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-       wrapped_bin=$out/bin/$(basename $bin)
-       echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
-       chmod +x $wrapped_bin
-      done
-    '';
+  utils = import ./utils.nix {
+    inherit pkgs;
+    inherit lib;
+    inherit config;
+  };
 in
 {
   home.username = specialArgs.username;
@@ -53,7 +45,8 @@ in
     '')
 
     nixgl.nixGLIntel
-    (nixGLWrap pkgs.alacritty)
+
+    (utils.nixGLWrapIntel pkgs.alacritty)
 
     vim
     clang-tools
---
+++ home/utils.nix
@@ -0,0 +1,14 @@
+{  pkgs, lib, config, ... }: {
+  nixGLWrapIntel = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
+}
