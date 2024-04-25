2ce647cbb9b7f8fe1452347109346c640edadec3
Rahul <mail@rahul.one>
2024-04-25 11:29:46 -0700

    Wezterm configuration

--- home/dotfiles/config/wezterm/wezterm.lua
+++ home/dotfiles/config/wezterm/wezterm.lua
@@ -1,8 +1,27 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.color_scheme = "Gruvbox Dark (Gogh)"
+
+config.font = wezterm.font("DroidSansMono NFM")
+
+config.hide_tab_bar_if_only_one_tab = false
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.window_close_confirmation = "NeverPrompt"
+
+config.window_background_opacity = 0.9
+config.text_background_opacity = 0.8
+config.window_background_gradient = {
+	colors = { "#000000", "#222222" },
+	orientation = { Linear = { angle = -45.0 } },
+}
+
+config.keys = {
+	{
+		key = "F11",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
 
 return config
