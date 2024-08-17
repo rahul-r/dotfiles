3185e6c09c364071518a81776c63b3b2434d064b
rahul <jossb6cw@duck.com>
2024-08-16 22:17:36 -0700

    Add coreutils and shortcuts for rustrover and vscode

--- machines/darwin.nix
+++ machines/darwin.nix
@@ -21,7 +21,12 @@
       home-manager switch --flake ${config.home.homeDirectory}/.dotfiles#macbook
     '')
 
+    (pkgs.writeShellScriptBin "rustrover" "/Applications/RustRover.app/Contents/MacOS/rustrover")
+    (pkgs.writeShellScriptBin "rover" "/Applications/RustRover.app/Contents/MacOS/rustrover")
+    (pkgs.writeShellScriptBin "code" ''"/Applications/Visual Studio Code.app/Contents/MacOS/Electron"'')
+
     qemu
+    coreutils
   ];
 
   home.file = {
